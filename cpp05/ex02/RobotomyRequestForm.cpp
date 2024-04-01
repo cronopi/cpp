384cb8e7606dacd43f47a28aa59070f10cdc09b3
Roberto Castano Blanco <rcastano@c2r1s5.42madrid.com>
2024-04-01 16:01:34 +0200

    ex02 seguimos

--- cpp05/ex02/RobotomyRequestForm.cpp
+++ cpp05/ex02/RobotomyRequestForm.cpp
@@ -6,7 +6,7 @@
 /*   By: rcastano <rcastano@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 15:39:28 by rcastano          #+#    #+#             */
-/*   Updated: 2024/03/19 15:49:32 by rcastano         ###   ########.fr       */
+/*   Updated: 2024/04/01 16:00:40 by rcastano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,23 @@ RobotomyRequestForm::~RobotomyRequestForm()
 {
 }
 
-RobotomyRequestForm::RobotomyRequestForm(RobotomyRequestForm &src)
+RobotomyRequestForm::RobotomyRequestForm(RobotomyRequestForm &src): Aform("ShrubberyCreationForm", 145, 137), _target(src._target)
 {
+    std::cout << "RobotomyRequestForm Copy Constructor called to copy " << src.getName() <<
+	" into " << this->getName() << std::endl;
+
+	*this = src;
 }
 
 RobotomyRequestForm &RobotomyRequestForm::operator=(const RobotomyRequestForm &src)
 {
+    Aform::operator=(src);
+	this->_target = src._target;
+	return (*this);
 }
 
 RobotomyRequestForm::RobotomyRequestForm(std::string target)
 {
+	std::cout << "RobotomyRequestForm Constructor for target " << target << " called" << std::endl; //no debe imprimirse
+
 }
--- cpp05/ex02/RobotomyRequestForm.hpp
+++ cpp05/ex02/RobotomyRequestForm.hpp
@@ -6,7 +6,7 @@
 /*   By: rcastano <rcastano@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 15:34:03 by rcastano          #+#    #+#             */
-/*   Updated: 2024/03/19 15:53:31 by rcastano         ###   ########.fr       */
+/*   Updated: 2024/04/01 15:57:25 by rcastano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,18 @@
 #ifndef ROBOTOMYREQUESTFORM_HPP
 # define ROBOTOMYREQUESTFORM_HPP
  
-class RobotomyRequestForm
+class RobotomyRequestForm : public Aform
 {
     private:
-
+        std:string _target;
     public:
         RobotomyRequestForm();
         ~RobotomyRequestForm();
         RobotomyRequestForm(RobotomyRequestForm &src);
-        &RobotomyRequestForm::operator=(const RobotomyRequestForm &src)
+        RobotomyRequestForm &RobotomyRequestForm::operator=(const RobotomyRequestForm &src)
 
         RobotomyRequestForm(std::string target);
+        void execute(Bureaucrat const &executor) const;
 };
 
 #endif
