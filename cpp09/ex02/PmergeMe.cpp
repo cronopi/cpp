489e36f56970a9bf0a06d648c8ca3cfbb0a8b77c
roberto <robertocast098@hotmail.com>
2024-10-29 16:51:00 +0100

    sorted minary missing

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -20,122 +20,6 @@ PmergeMe &PmergeMe::operator=(const PmergeMe &rhs)
 	return (*this);
 }
 
-
-template <typename Container>
-void PmergeMe::print(const Container& container, const std::string& tipo, std::string display)
-{
-	std::cout << tipo << " =>\n";
-	 std::cout << display << ": ";
-    for (typename Container::const_iterator it = container.begin(); it != container.end(); it++)
-	{
-        std::cout << *it << " ";
-	}
-	std::cout << std::endl;
-	std::cout << std::endl;
-}
-
-void PmergeMe::SortList(std::list<int> arr)
-{
-    std::list<std::pair<int, int>> pares;
-    std::list<std::pair<int, int>> sorted_pares;
-    int tmp = 0;
-    int tmp_it = 0;
-    int straggler = -1;
-    (void)straggler;
-	if (arr.size() <= 1)
-		return;
-
-    if ((arr.size() % 2) != 0)
-    {   
-        straggler = arr.back();
-        std::cout << "straggler: " << straggler << std::endl;
-        arr.pop_back();
-        print(arr, "list", "arreglada");
-    }
-    for (std::list<int>::iterator it = arr.begin(); it != arr.end();)
-    {
-        pares.emplace_back(*it, *std::next(it));
-        it++;
-        it++;
-    }
-    for (auto& par : pares) 
-    {
-        if (par.first > par.second)
-        {
-            tmp = par.first;
-            par.first = par.second;
-            par.second = tmp;
-        }
-    }
-    for (auto& par : pares) 
-    {
-        std::cout << "(" << par.first << ", " << par.second << ")" << std::endl;
-    }
-	std::cout << std::endl;
-
-
-    while (pares.size() > 0)
-    {
-        tmp = 2147483647;
-        int tmp2 = 2147483647;
-        std::list<std::pair<int, int>>::iterator it;
-        for (it = pares.begin(); it != pares.end(); it++)
-        {
-            if (tmp > it->second)
-            {
-                tmp = it->second;
-                tmp2 = it->first;
-                tmp_it = std::distance(pares.begin(), it);
-            }
-            std::cout << "(" << it->first << ", " << it->second << ")" << std::endl;
-        }
-
-        sorted_pares.emplace_back(tmp2, tmp);
-        pares.erase(tmp_it);
-        it = pares.begin();
-
-        std::cout << "sorted_pares: " << std::endl;
-        for (std::list<std::pair<int, int>>::iterator it = sorted_pares.begin(); it != sorted_pares.end(); it++)
-        {
-            std::cout << "(" << it->first << ", " << it->second << ")" << std::endl;
-        }
-    }
-
-}
-
-void PmergeMe::merge(std::list<int>& left, std::list<int>& right, std::list<int>& arr)
-{
-    std::list<int>::iterator it1 = left.begin();
-    std::list<int>::iterator it2 = right.begin();
-
-    while (it1 != left.end() && it2 != right.end())
-    {
-        if (*it1 <= *it2)
-        {
-            arr.push_back(*it1);
-            ++it1;
-        }
-        else
-        {
-            arr.push_back(*it2);
-            ++it2;
-        }
-    }
-
-    while (it1 != left.end())
-    {
-        arr.push_back(*it1);
-        ++it1;
-    }
-
-    while (it2 != right.end())
-    {
-        arr.push_back(*it2);
-        ++it2;
-    }
-}
-
-
 PmergeMe::PmergeMe(char **argv)
 {
     int tmp = 0;
@@ -156,3 +40,157 @@ PmergeMe::PmergeMe(char **argv)
 
 	SortList(lst);
 }
+
+template <typename Container>
+void PmergeMe::print(const Container& container, const std::string& tipo, std::string display)
+{
+	std::cout << tipo << " =>\n";
+	 std::cout << display << ": ";
+    for (typename Container::const_iterator it = container.begin(); it != container.end(); it++)
+	{
+        std::cout << *it << " ";
+	}
+	std::cout << std::endl;
+	std::cout << std::endl;
+}
+
+std::list<int> PmergeMe::build_jacob_insertion_sequence(int length)
+{
+    std::list<int> sequence;
+    sequence.push_back(1);
+    sequence.push_back(3);
+
+    int a = 1;
+    int b = 3;
+    int next;
+    for (int i = 2; i < length; i++)
+    {
+        next = a + b + 1;
+        sequence.push_back(next);
+        a = b;
+        b = next;
+    }
+
+    return (sequence);
+}
+
+void PmergeMe::Sortbypair(std::list<std::pair<int, int>> sorted_pares)
+{
+    std::list<int> s;
+    std::list<int> pend;
+    std::list<int> insertion_sequence;
+
+    for(std::list<std::pair<int, int>>::iterator it = sorted_pares.begin(); it != sorted_pares.end(); it++)
+    {
+        pend.push_back(it->first);
+        s.push_back(it->second);
+    }
+    s.push_front(pend.front());
+    pend.pop_front();
+    print(pend, "list", "pend");
+    print(s, "list", "s");
+
+    insertion_sequence = build_jacob_insertion_sequence(pend.size());
+
+    print(insertion_sequence, "list", "insertion_sequence");
+
+    int i = 0;
+    for (auto it = pend.begin(); it != pend.end(); ++it)
+    {
+        auto insertion_it = insertion_sequence.begin();
+        std::advance(insertion_it, i); // avanzar el iterador a la posición i
+        int index = *insertion_it;
+        auto insert_it = s.begin();
+        std::advance(insert_it, index); // move the iterator to the correct position
+        s.insert(insert_it, *it);
+        i++;
+    }
+
+    /*
+    Una búsqueda binaria es un algoritmo de búsqueda que se utiliza para encontrar un elemento específico en una lista ordenada.
+    El algoritmo funciona dividiendo la lista en dos partes y comparando el elemento que se busca con el elemento del medio de la lista.
+
+    Aquí hay los pasos básicos de una búsqueda binaria:
+
+    Se inicia con una lista ordenada de elementos.
+    Se calcula el índice del medio de la lista.
+    Se compara el elemento que se busca con el elemento del medio de la lista.
+    Si el elemento que se busca es igual al elemento del medio, se ha encontrado el elemento y se devuelve su índice.
+    Si el elemento que se busca es menor que el elemento del medio, se repite el proceso con la mitad izquierda de la lista.
+    Si el elemento que se busca es mayor que el elemento del medio, se repite el proceso con la mitad derecha de la lista.
+    Se repite el proceso hasta que se encuentre el elemento o se determine que no está en la lista.
+    */
+    print(s, "final", "s");
+    /*
+    if (straggler != -1)
+    {
+
+    } */
+
+
+}
+
+void PmergeMe::SortList(std::list<int> arr)
+{
+    std::list<std::pair<int, int>> pares;
+    std::list<std::pair<int, int>> sorted_pares;
+    int tmp = 0;
+    straggler = -1;
+	if (arr.size() <= 1)
+		return;
+
+    if ((arr.size() % 2) != 0)
+    {
+        straggler = arr.back();
+        std::cout << "straggler: " << straggler << std::endl;
+        arr.pop_back();
+        print(arr, "list", "arreglada");
+    }
+    for (std::list<int>::iterator it = arr.begin(); it != arr.end();)
+    {
+        pares.emplace_back(*it, *std::next(it));
+        it++;
+        it++;
+    }
+    for (auto& par : pares)
+    {
+        if (par.first > par.second)
+        {
+            tmp = par.first;
+            par.first = par.second;
+            par.second = tmp;
+        }
+    }
+/*     for (auto& par : pares)
+    {
+        std::cout << "(" << par.first << ", " << par.second << ")" << std::endl;
+    } */
+	std::cout << std::endl;
+    while (pares.size() > 0)
+    {
+        tmp = 2147483647;
+        int tmp2 = 2147483647;
+        std::list<std::pair<int, int>>::iterator it;
+        std::list<std::pair<int, int>>::iterator it_tmp;
+        for (it = pares.begin(); it != pares.end(); it++)
+        {
+            if (tmp > it->second)
+            {
+                tmp = it->second;
+                tmp2 = it->first;
+                it_tmp = it;
+            }
+            //std::cout << "(" << it->first << ", " << it->second << ")" << std::endl;
+        }
+        sorted_pares.emplace_back(tmp2, tmp);
+        pares.erase(it_tmp);
+        it = pares.begin();
+    }
+        std::cout << "sorted_pares: " << std::endl;
+        for (std::list<std::pair<int, int>>::iterator it = sorted_pares.begin(); it != sorted_pares.end(); it++)
+        {
+            std::cout << "(" << it->first << ", " << it->second << ")" << std::endl;
+        }
+        Sortbypair(sorted_pares);
+}
+
--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -11,21 +11,24 @@ class PmergeMe
 	private:
 		std::vector<int>	vec;
 		std::list<int>		lst;
-
+		int straggler;
 	public:
 		PmergeMe();
 		~PmergeMe();
 		PmergeMe(const PmergeMe &copy);
 		PmergeMe &operator=(const PmergeMe &rhs);
 
+		std::list<int> build_jacob_insertion_sequence(int length);
 		PmergeMe(char **argv);
 		void SortList(std::list<int> arr);
-		void merge(std::list<int>& left, std::list<int>& right, std::list<int>& arr);
-		void SortVector(std::vector<int> vec);
+		void Sortbypair(std::list<std::pair<int, int>> sorted_pares);
+
+
 		template <typename Container>
-    	void print(const Container& container, const std::string& tipo, std::string display);
+		void print(const Container& container, const std::string& tipo, std::string display);
+
+
+		void SortVector(std::vector<int> vec);
 };
 
 #endif
-
-// ./MergeMe 1 2 4
